2f5e6cd1f65d5bd7e7b7386ef3f4ecb8a0de27f7
Mateo Brnić <mateo.brnic@gmail.com>
2024-10-13 18:56:57 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiEpubTTSReader
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
